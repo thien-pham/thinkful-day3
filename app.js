74127f49afc0165e472096fd6e6ded80073bc839
pham-thien <pham.thien@outlook.com>
2017-04-05 12:16:43 -0400

    add object drills

---
+++ app.js
@@ -0,0 +1,47 @@
+// function createMyObject() {
+// 	return {
+// 		foo: 'bar', 
+// 		answerToUniverse: 42, 
+// 		'olly olly': 'oxen free', 
+// 		sayHello: function() {
+// 			return 'hello';
+// 		}
+// 	};
+// }
+
+// function updateObject(obj) {
+// 	obj.foo = 'foo';
+// 	obj.bar = 'bar';
+// 	obj.bizz = 'bizz';
+// 	obj.bang = 'bang';
+// 	return obj;
+// }
+
+// function personMaker() {
+//   var person = {
+//     firstName: 'Paul',
+//     lastName: 'Jones',
+
+//     fullName: function() {
+//     	return this.firstName + ' ' + this.lastName;
+//     }
+//   };
+//   return person;
+// }
+
+// var personLog = personMaker();
+// // when you call method without () it'll show you actual function, if you use () it'll run the function
+// console.log(personLog.fullName());
+
+function keyDeleter(obj) {
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+} 
+
+var sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang'
+};
