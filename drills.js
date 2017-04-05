079074f955670d15e15c731786ab0c08c9762a03
pham-thien <pham.thien@outlook.com>
2017-04-05 16:06:35 -0400

    add drills js

---
+++ drills.js
@@ -0,0 +1,27 @@
+// const loaf = {
+// 	flour: 300,
+// 	water: 210,
+// 	hydration: function() {
+// 		return this.water / this.flour * 100;
+// 	}
+// }
+
+// console.log(loaf.hydration());
+
+// const newObject = {
+// 	foo: 'foo',
+// 	bar: 'bar',
+// 	fum: 'fum',
+// 	quux: 'quux',
+// 	spam: 'spam'
+// }
+
+// for(let key in newObject) {
+// 	console.log(`${key}: ${newObject[key]}`);
+// }
+
+const dailyLife = { 
+	meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper']
+}
+
+console.log(dailyLife.meals[3]);
